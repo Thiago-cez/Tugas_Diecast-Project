6d4b45e8e14f67690297c4e28a4b6328eb95d3ff
Thiago-Cez <thiago.silva.ts804@gmail.com>
2023-07-09 14:01:26 +0100

    update in API

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,28 @@
 const express = require('express');
-const server = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
 const mainLineA2023 = require('./src/data/mainLineA2023.json');
 const mainLineB2023 = require('./src/data/mainLineB2023.json');
 const mainLineC2023 = require('./src/data/mainLineC2023.json');
 
 
-server.get('/', (req,res) => {
+app.get('/', (req,res) => {
     return res.json({mensagem: "Our API IS WORKING!!!"})
 });
 
-server.get('/A', (req,res) => {
+app.get('/A', (req,res) => {
     return res.json(mainLineA2023);
 });
 
-server.get('/B', (req,res) => {
+app.get('/B', (req,res) => {
     return res.json(mainLineB2023);
 });
 
-server.get('/C', (req,res) => {
+app.get('/C', (req,res) => {
     return res.json(mainLineC2023);
 });
 
-server.listen(3000, () => {
-    console.log("server Working!!!")
+app.listen(port, () => {
+    console.log("servidor rodando!!s")
 });
